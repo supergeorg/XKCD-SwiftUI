18a381b6ecd0e0e43b18fc5ad4c32c745e400c41
Georg Meißner <Georg.Meissner@mailbox.tu-dresden.de>
2020-03-02 14:25:50 +0100

    Initial Commit

---
+++ XKCD/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  XKCD
+//
+//  Created by Georg Meissner on 02.03.20.
+//  Copyright © 2020 Georg Meissner. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
