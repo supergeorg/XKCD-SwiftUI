a716ef7cfa27f99b27d682e113a229b7929f8743
Georg Meißner <Georg.Meissner@mailbox.tu-dresden.de>
2020-06-24 13:51:42 +0200

    Initial Commit

---
+++ Shared/XKCDApp.swift
@@ -0,0 +1,17 @@
+//
+//  XKCDApp.swift
+//  Shared
+//
+//  Created by Georg Meissner on 24.06.20.
+//
+
+import SwiftUI
+
+@main
+struct XKCDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
